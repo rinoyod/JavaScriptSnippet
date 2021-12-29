f74867367cd5f58aea0961ffd88013ec7be06832
rinotoyod <rinotoyod@gmail.com>
2021-12-29 18:28:01 +0900

    download追加

---
+++ js/download.js
@@ -0,0 +1,16 @@
+﻿/**
+ * 文字列を形式を指定してダウンロードする
+ * @param {string} filename 
+ * @param {string} str 
+ * @param {string} mimeType   'text/plain' ,'text/html'...etc
+ */
+function download(filename, str, mimeType){
+    const blob = new Blob([str], {"type": mimeType}); 
+    const a = document.createElement("a");
+    a.href = URL.createObjectURL(blob);
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(a.href);
+}
